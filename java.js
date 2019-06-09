1209d80ad8a0859cf0ae402698f480be78608ab9
alex6444 <alex@jansen.net.au>
2019-06-09 15:16:28 +1000

    Add files via upload

---
+++ java.js
@@ -0,0 +1,15 @@
+function check_password (input_element) {
+  
+    //get value of input
+    var password = input_element.value;
+    
+    //check value and show/hide the div
+    if (password == '1234')
+        
+        document.getElementById ('hidden_div').style.display = 'block';
+
+    else
+    
+        document.getElementById ('hidden_div').style.display = 'none';
+  
+  }
